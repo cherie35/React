80a9fe2b635707693ed9f5f0767e1872f5a65f0a
Chérie <cherie-@hotmail.nl>
2019-06-03 04:11:01 +0200

    yaaaaas

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar.js';
+import Me from './pages/Me.js';
 import Main from './pages/Main.js';
 import Projects from './pages/Projects.js';
 import About from './pages/About.js';
 import Interests from './pages/Interests.js';
 import { Link, animateScroll as scroll } from "react-scroll";
 import './App.css';
+
 
 const navstyle ={
   position: 'fixed',
@@ -26,6 +27,7 @@ function App() {
     <Projects/>
     <About/>
     <Interests/>
+    <Me/>
     
     
     </div>
--- portfolio/src/pages/Main.js
+++ portfolio/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ReactComponent as Github} from "../assets/images/github.svg"
+import { ReactComponent as Message} from "../assets/images/message.svg"
 import { ReactComponent as Insta} from "../assets/images/instagram.svg"
-import { ReactComponent as LinkedIn} from "../assets/images/linkedin_square.svg"
+import { ReactComponent as LinkedIn} from "../assets/images/linkedin.svg"
 import Button from 'react-bootstrap/Button';
 import Particles from 'react-particles-js';
 import {Link} from "react-scroll";
@@ -17,10 +18,11 @@ function Main() {
       <Cell col={12}>
         <div className="mainPageText">
           <h1>Hello, I'm Chérie Cederboom</h1>
-          <p>Computer Science student</p>
+          <p>Computer Science student &nbsp; | &nbsp; Aspiring Front-end developer</p>
         </div>
 
-      <Cell col={12} className="mainButtons">
+      <Cell class="mdl-cell--middle mdl-layout-spacer" col={9} className="mainButtons">
+      
       <Link
 				activeClass="active"
 				to="projectsPage"
@@ -33,7 +35,7 @@ function Main() {
         </Button>  
       </Link>
       <Button className={"buttonSub"} href="mailto:cherie-@hotmail.nl">
-        Get In Touch
+        <Message style={{width:'1.5rem', marginRight:'10px', paddingBottom: '5px'}}/>Get In Touch
         </Button>
       </Cell>
 
