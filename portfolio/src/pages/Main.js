f9811df2011bda26fc0fdea4fcec47cebba4e486
Chérie <cherie-@hotmail.nl>
2019-06-03 02:46:25 +0200

    main nearly finished

--- portfolio/src/pages/Main.js
+++ portfolio/src/pages/Main.js
@@ -4,29 +4,38 @@ import { ReactComponent as Insta} from "../assets/images/instagram.svg"
 import { ReactComponent as LinkedIn} from "../assets/images/linkedin_square.svg"
 import Button from 'react-bootstrap/Button';
 import Particles from 'react-particles-js';
-
+import {Link} from "react-scroll";
+import {Grid, Cell} from 'react-mdl';
 
 function Main() {
   return (
 
-    <div className="mainPage" >
-        <div
-    style={{
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%"
-    }}
-  ><Particles /></div>
-      
-      <div className="inner"><span>
-      <h1>Lorem ipsum dolor sit amet,</h1>
-      <p>Computer Science student</p>
-      <Button className={"buttonMain"} variant={"primary"}>See Past Work</Button>  <Button className={"buttonSub"}>Get In Touch</Button>
-      <div className="logosMain" ><Github /> <Insta /> <LinkedIn/></div>
-      </span>
-      </div>
+    <div className="mainPage" style={{width: '100%', margin: 'auto'}} >
+
+      <Grid className="inner">
+        <Cell col={12}>
+          <div className="mainPageText">
+        <h1>I'm Chérie Cederboom</h1>
+      <p>2nd year Computer Science student</p>
+        </div>
+      <Cell col={12} className="mainButtons">
+      <Link
+				activeClass="active"
+				to="projectsPage"
+				spy={true}
+				smooth={true}
+				duration= {500}
+			>
+      <Button className={"buttonMain"} >See Past Work</Button>  
+      </Link>
+      <Button className={"buttonSub"} href="mailto:cherie-@hotmail.nl">Get In Touch</Button>
+      </Cell>
+      <div  className="logosMain" >
+        <Github/> 
+        <Insta /> 
+        <LinkedIn/></div>
+      </Cell>
+      </Grid>
     </div>
   );
 }
