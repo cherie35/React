6c32dbb6341bd32d0697e2bcf931e54cfa3a1198
Chérie <cherie-@hotmail.nl>
2019-06-04 22:49:48 +0200

    - "how many edits u got?"  - a lot.

--- portfolio/src/pages/Me.js
+++ portfolio/src/pages/Me.js
@@ -11,22 +11,19 @@ function Me() {
           <Cell className="abt" col={12}>
           <img className='headshot' src={Head}></img>
           <p>
-            Real ass bitch, give a fuck 'bout a nigga <br/>
-            Big Birkin bag, hold five, six figures<br/>
-            Stripes on my ass so he call this pussy Tigger<br/>
-            Fuckin' on a scammin' ass, rich ass nigga<br/>
-            Same group of bitches, ain't no adding to the picture<br/>
-            Drop a couple racks, watch this ass get bigger<br/>
-            Drinkin' on liquor, and I'm lookin' at your nigga<br/>
-            If his money right, he can eat it like a Snicker<br/>
-            I ain't got time for you fake ass hoes<br/>
-            Talkin' all loud in them fake ass clothes<br/>
-            Fake ass shoes match their fake ass gold<br/>
-            I'm the realest bitch ever to you snake ass hoes<br/>
-            Act up, you can get snatched up<br/>
-            Act up, you can get snatched up<br/>
-            Act up, you can get snatched up<br/>
-            Dirty ass nails, baby girl, you need to back up<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            bsf;gvbdfbvfbvdbvdhbavdhbhldvbhalvbhavb;ba<br/>
+            
           </p>
           </Cell>
           </Grid>
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import {Grid, Cell} from 'react-mdl';
+import Placeholder from '../assets/images/placeholder-image.png';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 function Projects() {
     return (
   
       <div className="projectsPage">
 
+      <Grid className='cont'>
+      <Cell col={12}>
       <h1 className='PageDesc'>projects</h1>
+
+      </Cell>
+      <Cell col={6} className="halfRight"></Cell>
+      <Cell col={6} className='colLeft'></Cell>
+      <Cell col={4}>
+        <Card >
+          <Card.Img src={Placeholder}/>
+          <Card.Body>
+            <Card.Title>Title</Card.Title>
+          </Card.Body>
+          <Card.Body>
+          </Card.Body>
+        </Card>
+        </Cell>
+        <Cell col={4}>
+        <Card >
+          <Card.Img src={Placeholder}/>
+          <Card.Body>
+            <Card.Title>Title</Card.Title>
+          </Card.Body>
+          <Card.Body>
+          </Card.Body>
+        </Card>
+        </Cell>
+        <Cell col={4}>
+        <Card>
+          <Card.Img src={Placeholder}/>
+          <Card.Body>
+            <Card.Title>Title</Card.Title>
+
+          </Card.Body>
+          <Card.Body>
+
+          </Card.Body>
+        </Card>
+      </Cell>
+      </Grid>
+
+      
 
       </div>
     );
